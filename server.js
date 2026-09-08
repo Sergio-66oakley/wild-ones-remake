bee7c7282f718010aebd4d9531a738d6f658e0bf
Sergio-66oakley <du7que91@gmail.com>
2026-09-07 21:31:12 -0500

    server.js

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Wild Ones Remake</title>
-      </head>
-
-      <body style="font-family:Arial;text-align:center;padding:40px">
-        <h1>Wild Ones Remake</h1>
-        <h2>Beta 0.1</h2>
-
-        <p>Servidor funcionando correctamente.</p>
-
-        <p>Monedas: <strong>999999</strong></p>
-        <p>Galletas: <strong>999999</strong></p>
-      </body>
-    </html>
-  `);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
